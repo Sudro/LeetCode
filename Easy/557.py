67b9fa1b163fd009f53a9a715740a7a5b0e822e6
Ilya Bystrikov <thesudro@gmail.com>
2024-04-30 23:57:26 +0300

    Addition to solving an easy task 557

--- Easy/557.py
+++ Easy/557.py
@@ -1,6 +1,11 @@
 # O(n^2):
 class Solution(object):
     def reverseWords(self, s):
+        return " ".join(elem[::-1] for elem in s.split())
+    
+# O(n^2):
+class Solution(object):
+    def reverseWords(self, s):
         d = s.split()
         for i in range(len(d)):
             d[i] = d[i][::-1]
